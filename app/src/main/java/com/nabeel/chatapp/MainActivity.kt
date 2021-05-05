0ae0b11ba3568feb74afd50ec9a1c000ea69303f
Nabeel <nabeelbaig962@gmail.com>
2021-05-05 22:20:45 +0300

    Testing the firebase implementation.

--- app/src/main/java/com/nabeel/chatapp/MainActivity.kt
+++ app/src/main/java/com/nabeel/chatapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.nabeel.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Write message to the database.
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello World!")
     }
 }
