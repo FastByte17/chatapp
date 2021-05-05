aeea96f76980098ce4e75d2c366e3d99d511df07
Nabeel <nabeelbaig962@gmail.com>
2021-05-06 02:09:35 +0300

    Part 2 complete, added the adapter and it's xml (bugged)
    In order to display the text on the screen (bugged) and check if it works with firebase.

--- app/src/main/java/com/nabeel/chatapp/MainActivity.kt
+++ app/src/main/java/com/nabeel/chatapp/MainActivity.kt
@@ -1,19 +1,81 @@
 package com.nabeel.chatapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var messages: ArrayList<String>
+    private lateinit var database: DatabaseReference
+    private lateinit var edMessage: EditText
+    private lateinit var rcMessageList: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edMessage = findViewById(R.id.messageText)
+        rcMessageList = findViewById(R.id.messageList)
+
+        database = Firebase.database.reference
+        messages = arrayListOf()
+
+        edMessage.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP){
+                addMessage()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
+
+        val messageListener = object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.value != null) {
+                    val messagesFromDatabase = (snapshot.value as HashMap<String,ArrayList<String>>).get("messages")
+                    messages.clear()
+                    messagesFromDatabase?.forEach {
+                        if (it != null) messages.add(it)
+                    }
+                    rcMessageList.adapter?.notifyDataSetChanged()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("Chat",error.toString())
+            }
+        }
 
         //Write message to the database.
         val database = Firebase.database
         val myRef = database.getReference("message")
 
         myRef.setValue("Hello World!")
+    }
+
+    private fun addMessage() {
+        val newMessage = edMessage.text.toString()
+        messages.add(newMessage)
+        database.child("messages").setValue(messages)
+        edMessage.setText("")
+        closeKeyboard()
+    }
+
+    private fun closeKeyboard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken,0)
+        }
     }
 }
